3be6130a2af361191dbe9713f6db9ec4b377b440
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2025-08-06 14:46:05 +0300

    feat: add navigation and connect routes with components

--- Sirma-Movie-App/src/App.jsx
+++ Sirma-Movie-App/src/App.jsx
@@ -1,7 +1,25 @@
+import { Routes, Route } from 'react-router'
+
+import { Home } from './components/Home/Home'
+import { Movies } from './components/Movies/Movies'
+import { Actors } from './components/Actors/Actors'
+import { Navigation } from './components/Navigation/Navigation'
+
 function App() {
   return (
     <>
-      <h1>Movie App</h1>
+      <header>
+        <Navigation />
+      </header>
+      <main>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/actors" element={<Actors />} />
+        </Routes>
+      </main>
+      <footer></footer>
+
     </>
   )
 }
