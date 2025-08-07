e54ba90833113759eec78aa6b67eecffff414ca1
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2025-08-07 14:30:46 +0300

    fix: each child in list should have unique key prop
    Added missing key into map function

--- Sirma-Movie-App/src/components/Movies/Movies.jsx
+++ Sirma-Movie-App/src/components/Movies/Movies.jsx
@@ -3,14 +3,14 @@ import { MovieContext } from '../../contexts/MovieContext';
 
 export const Movies = () => {
     const { data, moviesMappedWithRoles } = useContext(MovieContext);
-
+    
     return (
         <ul>
             {data.movies.map(currentMovie => (
                 <div key={currentMovie.ID}>
                     <li>{currentMovie.Title}</li>
-                    {moviesMappedWithRoles[currentMovie.ID]?.map((currentRole) => (
-                        <p>{currentRole.RoleName} - {data.actors.find(actor => actor.ID == currentRole.ActorID).FullName}</p>
+                    {moviesMappedWithRoles[currentMovie.ID]?.map((currentRole, index) => (
+                        <p key={index} >{currentRole.RoleName} - {data.actors.find(actor => actor.ID == currentRole.ActorID).FullName}</p>
                     ))}
                 </div>
             ))}
