773e3afb6baf049c71eaa27d7435cfa40481feed
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2025-08-08 17:59:53 +0300

    feat: implement actors page functionality

--- Sirma-Movie-App/src/components/Actors/Actors.jsx
+++ Sirma-Movie-App/src/components/Actors/Actors.jsx
@@ -1,5 +1,21 @@
+import { useState, useEffect, useContext } from 'react'
+import { MovieContext } from '../../contexts/MovieContext';
 export const Actors = () => {
+    const { data, dataById, actorsMappedWithRoles } = useContext(MovieContext);
+
     return (
-        <h1>Actors</h1>
+        <ul>
+            {data.actors.map(currentActor => (
+                <li key={currentActor.ID}>
+                    {currentActor.FullName}
+                    <h3>Movies:</h3>
+                    {actorsMappedWithRoles[currentActor.ID]?.map((currentElement => (
+                        <p key={`${currentActor.ID} - ${currentElement.MovieID}`}>
+                            {dataById.moviesObj[currentElement.MovieID].Title} - {currentElement.RoleName}
+                        </p>
+                    )))}
+                </li>
+            ))}
+        </ul>
     )
 }
