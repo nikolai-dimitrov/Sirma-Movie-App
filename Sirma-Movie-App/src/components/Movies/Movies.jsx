1c589b9c712e05c3a99a7a49b1653801d4878066
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2025-08-08 15:14:29 +0300

    refactor: replace .find in jsx with predefine object
    Instead of using .find() in jsx for every actor to access detailed information for every role, created actorsById object and access detailed actor information by key in jsx

--- Sirma-Movie-App/src/components/Movies/Movies.jsx
+++ Sirma-Movie-App/src/components/Movies/Movies.jsx
@@ -3,14 +3,15 @@ import { MovieContext } from '../../contexts/MovieContext';
 
 export const Movies = () => {
     const { data, moviesMappedWithRoles } = useContext(MovieContext);
-    
+    const actorsById = Object.fromEntries(data.actors.map((currentActor) => [currentActor.ID, currentActor]))
+
     return (
         <ul>
             {data.movies.map(currentMovie => (
                 <div key={currentMovie.ID}>
                     <li>{currentMovie.Title}</li>
                     {moviesMappedWithRoles[currentMovie.ID]?.map((currentRole, index) => (
-                        <p key={index} >{currentRole.RoleName} - {data.actors.find(actor => actor.ID == currentRole.ActorID).FullName}</p>
+                        <p key={index} >{currentRole.RoleName} - {actorsById[currentRole.ActorID].FullName}</p>
                     ))}
                 </div>
             ))}
