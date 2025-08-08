58ea4803b9ef5c43c35667d7751d3c48ce1bbd3b
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2025-08-08 15:37:58 +0300

    feat: implement functionality to show actors and movies where they acted in together

--- Sirma-Movie-App/src/components/Home/Home.jsx
+++ Sirma-Movie-App/src/components/Home/Home.jsx
@@ -1,12 +1,25 @@
-import { useEffect, useContext } from 'react'
+import { useContext } from 'react'
 import { MovieContext } from '../../contexts/MovieContext';
 export const Home = () => {
-    const { data} = useContext(MovieContext);
+    const { data, topActorPair } = useContext(MovieContext);
+
+    const [actorIdPair, movieIdsArray] = topActorPair;
+    const [firstActorId, secondActorId] = actorIdPair?.split("-") || [];
+
+    const [firstActor, secondActor] = data.actors.filter((el) => (el.ID == firstActorId || el.ID == secondActorId));
+    const moviesPlayed = movieIdsArray?.map((searchedMovieId) => data.movies.find((el) => el.ID == searchedMovieId))
+
+    const sharedMoviesCount = moviesPlayed?.length;
 
     return (
         <>
             <h1>Home</h1>
-            {/* <button onClick={addRole}>Add Role</button> */}
+            <p>Total Shared Movies: {sharedMoviesCount}</p>
+            <p>{firstActor?.FullName}</p>
+            <p>{secondActor?.FullName}</p>
+            {moviesPlayed?.map((currentMovie) => (
+                <p key={currentMovie.ID}>{currentMovie.Title}</p>
+            ))}
         </>
     )
 }
