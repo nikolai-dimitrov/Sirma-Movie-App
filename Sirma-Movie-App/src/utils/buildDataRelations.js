b6aa5c7a8aa528a689e9bf3d50a7bdc3998da8dc
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2025-08-09 11:49:30 +0300

    refactor: remove functionality to add two additional properties

--- Sirma-Movie-App/src/utils/buildDataRelations.js
+++ Sirma-Movie-App/src/utils/buildDataRelations.js
@@ -1,18 +1,11 @@
 // Iterate over the roles array and create object, which keys will be the returned value of currentRole[propertyName] and value -> array which contains role objects
-export const buildDataRelations = (roles, propertyName, collection) => {
+export const buildDataRelations = (roles, propertyName) => {
 	const dataRelation = {};
 
 	roles.forEach((currentRole) => {
 		const key = currentRole[propertyName];
 		if (!dataRelation[key]) {
 			dataRelation[key] = [];
-		}
-
-		// Seed additional data in roles
-		if (propertyName == "MovieID") {
-			currentRole["MovieName"] = collection[key].Title;
-		} else if (propertyName == "ActorID") {
-			currentRole["ActorName"] = collection[key].FullName;
 		}
 
 		dataRelation[key].push(currentRole);
