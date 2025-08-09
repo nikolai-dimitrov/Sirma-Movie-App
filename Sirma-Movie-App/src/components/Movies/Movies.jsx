536876bff92add6574c7fca272fe5be4899fb120
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2025-08-09 14:13:20 +0300

    refactor: component use moviesMappedWithRoles for rendering instead of data.movies

--- Sirma-Movie-App/src/components/Movies/Movies.jsx
+++ Sirma-Movie-App/src/components/Movies/Movies.jsx
@@ -2,19 +2,18 @@ import { useState, useEffect, useContext } from 'react'
 import { MovieContext } from '../../contexts/MovieContext';
 
 export const Movies = () => {
-    const { data, moviesMappedWithRoles, addMovie, addRole } = useContext(MovieContext);
+    const { moviesMappedWithRoles, addMovie, addRole } = useContext(MovieContext);
 
     return (
         <>
             <button onClick={addMovie}>Add Movie</button>
             <button onClick={addRole}>Add Role</button>
-
             <ul>
-                {data.movies.map(currentMovie => (
+                {moviesMappedWithRoles.map(currentMovie => (
                     <li key={currentMovie.ID}>
                         <p>{currentMovie.Title} - {currentMovie.ReleaseDate}</p>
-                        {moviesMappedWithRoles[currentMovie.ID]?.map((currentRole, index) => (
-                            <p key={index}>{currentRole.RoleName == 'null' ? 'Unnamed' : currentRole.RoleName} - {currentRole.ActorDetails.FullName}</p>
+                        {currentMovie.roles.map((currentRole, index) => (
+                            <p key={index}>{currentRole.RoleName == 'null' ? 'Unnamed' : currentRole.RoleName} - {currentRole.ActorDetails?.FullName}</p>
                         ))}
                     </li>
                 ))}
