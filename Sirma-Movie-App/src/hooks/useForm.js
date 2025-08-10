525358bfee2afae1eaafb4f9eb0e1f747e43e3f6
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2025-08-10 13:08:20 +0300

    feat: clear form on submit

--- Sirma-Movie-App/src/hooks/useForm.js
+++ Sirma-Movie-App/src/hooks/useForm.js
@@ -2,6 +2,15 @@ import { useState } from "react";
 export const useForm = (initialFormValues, submitHandler) => {
 	const [formValues, setFormValues] = useState(initialFormValues);
 
+	const clearFormValues = () => {
+		const formValuesCopy = { ...formValues };
+		Object.keys(formValues).forEach(
+			(currentKey) => (formValuesCopy[currentKey] = "")
+		);
+
+		setFormValues(formValuesCopy);
+	};
+
 	const onChangeHandler = (e) => {
 		setFormValues((prevState) => ({
 			...prevState,
@@ -12,6 +21,7 @@ export const useForm = (initialFormValues, submitHandler) => {
 	const onSubmitHandler = (e, id) => {
 		e.preventDefault();
 		id ? submitHandler(formValues, id) : submitHandler(formValues);
+		clearFormValues();
 	};
 
 	return { formValues, onChangeHandler, onSubmitHandler };
