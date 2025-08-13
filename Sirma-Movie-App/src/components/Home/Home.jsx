f9ca8a65449a7bc2f765587f8c518be42adb4f7f
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2025-08-13 12:11:09 +0300

    feat: implement very simple homepage ui

--- Sirma-Movie-App/src/components/Home/Home.jsx
+++ Sirma-Movie-App/src/components/Home/Home.jsx
@@ -3,6 +3,7 @@ import { MovieContext } from '../../contexts/MovieContext';
 
 import { getTopActorPair } from '../../utils//actorsPairMoviesMapper';
 
+import styles from './home.module.css'
 export const Home = () => {
     const { data, moviesMappedWithRoles } = useContext(MovieContext);
     const [topActorPair, setTopActorPair] = useState([]);
@@ -19,14 +20,30 @@ export const Home = () => {
     const sharedMoviesCount = moviesPlayed?.length;
 
     return (
-        <>
-            <h1>Home</h1>
-            <p>Total Shared Movies: {sharedMoviesCount}</p>
-            <p>{firstActor?.FullName}</p>
-            <p>{secondActor?.FullName}</p>
-            {moviesPlayed?.map((currentMovie) => (
-                <p key={currentMovie.ID}>{currentMovie.Title}</p>
-            ))}
-        </>
+        <section className={styles.home}>
+            <article>
+                <div className={styles.actorsInformationContainer}>
+                    <h4>Top pair of actors:</h4>
+                    <div>
+                        <p>{firstActor?.FullName}</p>
+                        <p>{secondActor?.FullName}</p>
+                    </div>
+                </div>
+                <ul className={styles.moviesInformationContainer}>
+                    <h4>Shared Movies List:</h4>
+                    {moviesPlayed?.map((currentMovie) => (
+                        <li key={currentMovie.ID}>
+                            <p>
+                                {currentMovie.Title}
+                            </p>
+                        </li>
+                    ))}
+                </ul>
+                <div className={styles.moviesCountContainer}>
+                    <h4>Shared Movies Count: </h4>
+                    <p>{sharedMoviesCount}</p>
+                </div>
+            </article>
+        </section>
     )
 }
