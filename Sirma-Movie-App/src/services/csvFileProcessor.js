dacddaec48fea732597a2517f5eb45918063a076
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2025-09-25 10:27:41 +0300

    refactor: add validation for csv file and default delimiter param
    Add validation for missing columns, rows and strings in csv file

--- Sirma-Movie-App/src/services/csvFileProcessor.js
+++ Sirma-Movie-App/src/services/csvFileProcessor.js
@@ -1,24 +1,41 @@
-const parseCsv = async (filePath) => {
+const parseCsv = async (filePath, delimiter = ",") => {
 	const fileData = await fetch(filePath);
 	const textFileData = await fileData.text();
 	const fileDataArray = textFileData.split("\n");
-
 	const [firstLine, ...restLines] = fileDataArray;
 
 	// trim because of \r last element
-	const keysArray = firstLine.trim().split(",");
-	const valuesArray = restLines;
-
+	const headerRow = firstLine.trim().split(delimiter);
+	const dataRows = restLines;
 	const parsedData = [];
 
-	valuesArray.forEach((currentRow, index) => {
+	dataRows.forEach((currentRow, index) => {
 		// trim because of \r last element
-		const currentRowArray = currentRow.trim().split(",");
+		const currentRowArray = currentRow.trim().split(delimiter);
 		const rowData = {};
+		const rowNumber = index + 2;
+
+		if (currentRowArray.length < headerRow.length) {
+			throw new Error(
+				`There is missing column at row: ${rowNumber}. File path: ${filePath}`
+			);
+		} else if (currentRowArray.length > headerRow.length) {
+			throw new Error(
+				`Expected columns: ${headerRow.length}, There are: ${currentRowArray.length} at row: ${rowNumber}. File path: ${filePath}`
+			);
+		}
 
 		currentRowArray.forEach((currentRowElement, index) => {
-			const key = keysArray[index];
+			const columNumber = index + 1;
+			if (currentRowElement == "") {
+				throw new Error(
+					`At row number: ${rowNumber}, column number: ${columNumber} is empty. File path: ${filePath}`
+				);
+			}
+
+			const key = headerRow[index];
 			rowData[key] = currentRowElement;
+
 			if (index == currentRowArray.length - 1) {
 				rowData[key] = formatDate(currentRowElement);
 			}
@@ -40,7 +57,7 @@ const formatDate = (dateString) => {
 };
 
 export const csvFileProcessor = {
-	getActors: () => parseCsv("/data/actors.csv"),
-	getMovies: () => parseCsv("/data/movies.csv"),
-	getRoles: () => parseCsv("/data/roles.csv"),
+	getActors: () => parseCsv("/data/actors.csv", ","),
+	getMovies: () => parseCsv("/data/movies.csv", ","),
+	getRoles: () => parseCsv("/data/roles.csv", ","),
 };
