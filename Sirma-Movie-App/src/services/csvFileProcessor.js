41be037918cd9c49999b028fab0466d6b0286a3f
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2025-09-26 14:37:49 +0300

    implement csv validation

--- Sirma-Movie-App/src/services/csvFileProcessor.js
+++ Sirma-Movie-App/src/services/csvFileProcessor.js
@@ -1,45 +1,61 @@
-const parseCsv = async (filePath, delimiter = ",") => {
+const parseCsv = async (filePath, delimiter = ",", singleColumn = false) => {
 	const fileData = await fetch(filePath);
 	const textFileData = await fileData.text();
 	const fileDataArray = textFileData.split("\n");
 	const [firstLine, ...restLines] = fileDataArray;
 
+	if (!singleColumn) validateDelimiter(fileDataArray, delimiter);
 	// trim because of \r last element
-	const headerRow = firstLine.trim().split(delimiter);
+	const headerRow = !singleColumn
+		? firstLine.trim().split(delimiter)
+		: [firstLine];
+
 	const dataRows = restLines;
 	const parsedData = [];
 
 	dataRows.forEach((currentRow, index) => {
-		// trim because of \r last element
-		const currentRowArray = currentRow.trim().split(delimiter);
 		const rowData = {};
 		const rowNumber = index + 2;
 
-		if (currentRowArray.length < headerRow.length) {
-			throw new Error(
-				`There is missing column at row: ${rowNumber}. File path: ${filePath}`
-			);
-		} else if (currentRowArray.length > headerRow.length) {
-			throw new Error(
-				`Expected columns: ${headerRow.length}, There are: ${currentRowArray.length} at row: ${rowNumber}. File path: ${filePath}`
-			);
-		}
+		if (!singleColumn) {
+			// trim because of \r last element
+			const currentRowArray = currentRow.trim().split(delimiter);
 
-		currentRowArray.forEach((currentRowElement, index) => {
-			const columNumber = index + 1;
-			if (currentRowElement == "") {
+			if (currentRowArray.length < headerRow.length) {
 				throw new Error(
-					`At row number: ${rowNumber}, column number: ${columNumber} is empty. File path: ${filePath}`
+					`There is missing column at row: ${rowNumber}. File path: ${filePath}`
+				);
+			} else if (currentRowArray.length > headerRow.length) {
+				throw new Error(
+					`Expected columns: ${headerRow.length}, There are: ${currentRowArray.length} at row: ${rowNumber}. File path: ${filePath}`
 				);
 			}
 
-			const key = headerRow[index];
-			rowData[key] = currentRowElement;
+			currentRowArray.forEach((currentRowElement, index) => {
+				const columNumber = index + 1;
+				if (currentRowElement == "") {
+					throw new Error(
+						`At row number: ${rowNumber}, column number: ${columNumber} is empty. File path: ${filePath}`
+					);
+				}
 
-			if (index == currentRowArray.length - 1) {
-				rowData[key] = formatDate(currentRowElement);
+				const key = headerRow[index];
+				rowData[key] = currentRowElement;
+
+				if (index == currentRowArray.length - 1) {
+					rowData[key] = formatDate(currentRowElement);
+				}
+			});
+		} else {
+			if (currentRow.trim() == "") {
+				throw new Error(
+					`There is missing column at row: ${rowNumber}. File path: ${filePath}`
+				);
 			}
-		});
+
+			const key = headerRow;
+			rowData[key] = currentRow.trim();
+		}
 
 		parsedData.push(rowData);
 	});
@@ -54,6 +70,17 @@ const formatDate = (dateString) => {
 	}
 
 	return date.toISOString().split("T")[0];
+};
+
+const validateDelimiter = (csvDataArray, delimiter) => {
+	const partialData = csvDataArray.splice(0, 10).map((line) => line.trim()).filter((el => el != ''));
+	const hasDelimiter = partialData.every((row) => row.includes(delimiter));
+
+	if (!hasDelimiter) {
+		throw new Error(
+			`Chosen delimiter - "${delimiter}" does not occur on every row in csv file.`
+		);
+	}
 };
 
 export const csvFileProcessor = {
